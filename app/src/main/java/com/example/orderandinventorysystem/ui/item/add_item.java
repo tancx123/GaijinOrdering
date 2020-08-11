89700db22718511f231bb89e24a2b22e110f76a2
tancx123 <tancx-pm17@student.tarc.edu.my>
2020-08-11 18:32:41 +0800

    commit with add new item

---
+++ app/src/main/java/com/example/orderandinventorysystem/ui/item/add_item.java
@@ -0,0 +1,16 @@
+package com.example.orderandinventorysystem.ui.item;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.orderandinventorysystem.R;
+
+public class add_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
